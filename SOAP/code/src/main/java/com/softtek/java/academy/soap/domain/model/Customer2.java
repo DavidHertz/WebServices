f4e59b87612a63ef1e0b81749ddfedc562692bc7
Antonio Hernandez <joseantonio.hernandezvazquez@staples.com>
2015-10-19 16:40:03 -0500

    [JAVA] Removing unnecessary  clases

--- SOAP/code/src/main/java/com/softtek/java/academy/soap/domain/model/Customer2.java
+++
@@ -1,8 +0,0 @@
-package com.softtek.java.academy.soap.domain.model;
-
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name = "getCustomerByIdResponse", namespace = "http://www.academy-softtek.com/soap")
-public class Customer2 {
-
-}
--- SOAP/code/src/main/java/com/softtek/java/academy/soap/domain/model/marshaller/ObjectFactory.java
+++ SOAP/code/src/main/java/com/softtek/java/academy/soap/domain/model/marshaller/ObjectFactory.java
@@ -1,6 +1,9 @@
-package com.softtek.java.academy.soap.domain.model;
+package com.softtek.java.academy.soap.domain.model.marshaller;
 
 import javax.xml.bind.annotation.XmlRegistry;
+
+import com.softtek.java.academy.soap.domain.model.BankAccountRequest;
+import com.softtek.java.academy.soap.domain.model.BankAccountResponse;
 
 @XmlRegistry
 public class ObjectFactory {
